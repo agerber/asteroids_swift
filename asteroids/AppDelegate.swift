4d812c75975413671559629cf13400e376a52311
Ankun <sytungan@gmail.com>
2024-09-30 22:16:36 +0700

    Initial Commit

---
+++ asteroids/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  asteroids
+//
+//  Created by Sy Tung An on 30/9/24.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
